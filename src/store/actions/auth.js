2dfc6e38afb684bdbf327a0e94d4522789e303d9
Leejoosu <joosublee@hotmail.com>
2019-08-29 22:32:15 -0400

    completed: persistent login, logout (both button and token timeout)

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,5 +1,6 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../axios-projects';
+import history from '../../history';
 
 export const login = (email, idToken, idTokenExpiryDate, isAdmin) => {
   return {
@@ -12,20 +13,53 @@ export const login = (email, idToken, idTokenExpiryDate, isAdmin) => {
 }
 
 export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('userId');
+  localStorage.removeItem('expireDate');
+  history.push('/');
+
   return {
     type: actionTypes.LOGOUT,
   }
 }
 
-export const authCheckState = () => {
-  console.log('inside sign in action')
+// export const authCheckState = () => {
+//   console.log('inside sign in action')
+//   return (dispatch) => {
+
+//     //if token did not expire, login
+
+//     // else logout
+//     dispatch({
+//       type: actionTypes.LOGOUT,
+//     })
+//   }
+// }
+
+export const checkTokenTimeout = (expiresIn) =>{
+  return dispatch => {
+    setTimeout(() => {
+      dispatch(logout());
+    }, expiresIn * 1000);//setTimeout expects miliseconds, so we are fixing our seconds into milliseconds
+  }
+}
+
+export const checkAuthToken = () => {
   return (dispatch) => {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      dispatch(logout());
+    } else  {
+      const expireDate = new Date(localStorage.getItem('expireDate'));
+      if (expireDate < new Date()) {
+        dispatch(logout());
+      } else {
+        const userEmail = localStorage.getItem('email');
+        
+        dispatch(login(userEmail, token, expireDate, false));        
+        dispatch(checkTokenTimeout( (expireDate.getTime() - (new Date().getTime()))/1000 ));
+      }
+    }
 
-    //if token did not expire, login
-
-    // else logout
-    dispatch({
-      type: actionTypes.LOGOUT,
-    })
   }
 }
