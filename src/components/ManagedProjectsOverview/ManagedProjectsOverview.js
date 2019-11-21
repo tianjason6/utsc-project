36c1448c196f91b9e94799d0878b21c311573e25
laiedgar <edgar.lai@hotmail.ca>
2019-11-20 22:35:56 -0500

    cleaned up Managed Projects Overview

--- src/components/ManagedProjectsOverview/ManagedProjectsOverview.js
+++ src/components/ManagedProjectsOverview/ManagedProjectsOverview.js
@@ -2,13 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import styles from "./ManagedProjectsOverview.module.css";
 import Project from "../Project/Project";
-import Project1Img from "../../assests/images/tempLogo.png";
-import AddProject1Img from "../../assests/images/addProject.svg";
-import * as featuredProjectsActions from "../../store/actions/featuredProjects";
 
 import * as userManagedProjectsActions from "../../store/actions/userManagedProjects";
-import * as authReducer from "../../store/reducers/auth";
-import * as userManagedProjectsReducer from "../../store/reducers/userManagedProjects";
 
 class ManagedProjectsOverview extends Component {
   componentDidMount() {
@@ -17,17 +12,10 @@ class ManagedProjectsOverview extends Component {
 
   render() {
     let managedProjects;
-    // need to get rid of console logs
-    // saved on database with null projects?
     if (this.props.userManagedProjects.length !== 0) {
-      console.log(
-        this.props.userManagedProjects.length,
-        "length of managed projects"
-      );
       managedProjects = this.props.userManagedProjects.map(project => {
-        console.log(project, "project variable in managed Projects");
         if (project === null) {
-          return undefined;
+          return null;
         }
         return (
           <Project
