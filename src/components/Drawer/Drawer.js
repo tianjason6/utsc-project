f5593ae364277ecc7f994051e20d9faf83fd2d09
laiedgar <edgar.lai@hotmail.ca>
2019-12-03 21:01:13 -0500

    changed loggedIn in Drawer from array to react fragment

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -20,25 +20,26 @@ class Drawer extends Component {
       this.props.closeDrawer();
       this.props.logout();
     };
-    const loggedIn = [
-      <div className={styles.title}>User Menu</div>,
-      <div className={styles.separator} />,
-      <NavLink onClick={this.props.closeDrawer} to={"/Console"}>
-        Console
-      </NavLink>,
-      <NavLink onClick={this.props.closeDrawer} to={"/test/TimeLine"}>
-        Timeline
-      </NavLink>,
-      <NavLink onClick={this.props.closeDrawer} to={"/test/ManagedProjects"}>
-        Managed Projects
-      </NavLink>,
-      <NavLink onClick={this.props.closeDrawer} to={"/test/JoinedProjects"}>
-        Joined Projects
-      </NavLink>,
-      <NavLink onClick={this.props.closeDrawer} to={"/test/ConsoleLogout"}>
-        Logout
+    const loggedIn = (
+      <>
+        <div className={styles.title}>User Menu</div>
+        <div className={styles.separator} />
+        <NavLink onClick={this.props.closeDrawer} to={"/Console"}>
+          Console
       </NavLink>
-    ];
+        <NavLink onClick={this.props.closeDrawer} to={"/test/TimeLine"}>
+          Timeline
+      </NavLink>
+        <NavLink onClick={this.props.closeDrawer} to={"/test/ManagedProjects"}>
+          Managed Projects
+      </NavLink>
+        <NavLink onClick={this.props.closeDrawer} to={"/test/JoinedProjects"}>
+          Joined Projects
+      </NavLink>
+        <NavLink onClick={this.props.closeDrawer} to={"/test/ConsoleLogout"}>
+          Logout
+      </NavLink>
+      </>)
     return (
       <>
         {this.props.isOpen ? (
@@ -69,8 +70,8 @@ class Drawer extends Component {
                 <div className={styles.NavItem}>Login</div>
               </a>
             ) : (
-              loggedIn
-            )}
+                loggedIn
+              )}
           </div>
         </div>
       </>
