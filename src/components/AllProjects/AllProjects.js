409b62ccbd7770df902cf3cc5aa1b4d0b808ee1c
laiedgar <edgar.lai@hotmail.ca>
2019-12-16 21:10:34 -0500

    added styling to all projects

--- src/components/AllProjects/AllProjects.js
+++ src/components/AllProjects/AllProjects.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import styles from "./AllProjects.module.css";
 import Project from '../Project/Project';
 import * as allProjectsActions from '../../store/actions/allProjects';
 
@@ -9,16 +10,17 @@ class AllProjects extends Component {
   }
 
   render() {
-    console.log("all projects component: ", this.props);
     return (
-      <div>
-        <div>
-          <h1>All Projects</h1>
-          <section>
-            {this.props.allProjects.map((project) => {
-              return <Project key={project.title} title={project.title} description={project.description} img={project.imgs[0]} projectInfo={project} />
-            })}
-          </section>
+      <div className={styles.Background}>
+        <div className={styles.Content}>
+          <div className={styles.AllProjects}>
+            <h1 className={styles.Title}>All Projects</h1>
+            <section className={styles.Wrap}>
+              {this.props.allProjects.map((project) => {
+                return <Project key={project.title} title={project.title} description={project.description} img={project.imgs[0]} projectInfo={project} />
+              })}
+            </section>
+          </div>
         </div>
       </div>
     );
