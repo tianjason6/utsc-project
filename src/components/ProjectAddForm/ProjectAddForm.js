cba57b0a19877991d34ce9ce48c851664d6dfa9f
Jason Tian <tianjason6@gmail.com>
2020-01-07 20:05:00 -0500

    minor bug fixes from before

--- src/components/ProjectAddForm/ProjectAddForm.js
+++ src/components/ProjectAddForm/ProjectAddForm.js
@@ -38,20 +38,8 @@ class ProjectAddForm extends Component {
     });
   }
 
-  updateFile1 = (file, id) => {
-    this.setState({ img1: file });
-  };
-
-  updateFile2 = (file, id) => {
-    this.setState({ img2: file });
-  };
-
-  updateFile3 = (file, id) => {
-    this.setState({ img3: file });
-  };
-
-  updateFile4 = (file, id) => {
-    this.setState({ img4: file });
+  updateFile = values => {
+    this.setState["img" + values.id] = values.file;
   };
 
   renderField = ({ input, label, type, meta: { touched, error } }) => (
@@ -97,7 +85,6 @@ class ProjectAddForm extends Component {
         }
 
         if (!hasImage) {
-          this.state.imgs = [""];
           alert("A project image is required!");
         } else {
           this.fileUploadHandler(values.title);
@@ -133,22 +120,22 @@ class ProjectAddForm extends Component {
                 <Field
                   name="imgs"
                   component={RenderImageField}
-                  onChange={this.updateFile1}
+                  onChange={this.updateFile}
                   id="1"
                 />
                 <Field
                   component={RenderImageField}
-                  onChange={this.updateFile2}
+                  onChange={this.updateFile}
                   id="2"
                 />
                 <Field
                   component={RenderImageField}
-                  onChange={this.updateFile3}
+                  onChange={this.updateFile}
                   id="3"
                 />
                 <Field
                   component={RenderImageField}
-                  onChange={this.updateFile4}
+                  onChange={this.updateFile}
                   id="4"
                 />
                 <div className={styles.containerSmall}></div>
