98579f6183bb8e8adda773c184d3b0bba123ce0b
SalikChodhary <50224612+SalikChodhary@users.noreply.github.com>
2020-03-07 14:32:18 -0500

    Update JoinedProjects.js

--- src/components/JoinedProjects/JoinedProjects.js
+++ src/components/JoinedProjects/JoinedProjects.js
@@ -18,28 +18,47 @@ class JoinedProjects extends Component {
     }
 
     render(){
-        let userJoinedProjects = <h1 className={styles.emptyMsg}>You haven't joined any projects! Go join some!</h1>;
+        let joinedProjects = <h1 className={styles.emptyMsg}>You haven't joined any projects! Go join some!</h1>;
+        if (this.props.userJoinedProjects.length !== 0) {
+            joinedProjects = this.props.userJoinedProjects.map(project => {
+              if (project === null) {
+                return null;
+              }
+              return (
+                <>
+                  <Project
+                    key={project.title}
+                    title={project.title}
+                    description={project.description}
+                    img={project.imgs[0]}
+                    projectInfo={project}
+                  />
+                </>
+              );
+            });
+          }
 
-        if(this.props.userJoinedProjects.length != 0){
-            // console.log("this si the props", this.props.userJoinedProjects);
-            return(
-                <div className={styles}>
-                    {this.props.userJoinedProjects.map((project) => (   
-                        <div className={styles}>
-                            {project.imgs !== undefined ? 
-                                <Project key={project.title}
-                                title={project.title}
-                                description={project.description}
-                                img={project.imgs[0]}
-                                projectInfo={project} />
-                            :
-                                <div></div>
-                            }
-                        </div>
-                    ))}
-                </div>
-            );
-        }
+        // if(this.props.userJoinedProjects.length != 0){
+        //     // console.log("this si the props", this.props.userJoinedProjects);
+        //     return(
+        //         <div className={styles}>
+        //             {this.props.userJoinedProjects.map((project) => (   
+        //                 <div className={styles}>
+        //                     {project.imgs !== undefined ? 
+        //                         <Project key={project.title}
+        //                         title={project.title}
+        //                         description={project.description}
+        //                         img={project.imgs[0]}
+        //                         projectInfo={project} />
+        //                     :
+        //                         <div></div>
+        //                     }
+        //                 </div>
+        //             ))}
+        //         </div>
+                
+        //     );
+        // }
         
         
         return(
@@ -48,7 +67,7 @@ class JoinedProjects extends Component {
                     <div className={styles.OngoingProjects}>
                         <h1>Joined Projects</h1>
                         <section className={styles.Wrap}>
-                            {userJoinedProjects}
+                            {joinedProjects}
                         </section>
                     </div>
                 </div>
