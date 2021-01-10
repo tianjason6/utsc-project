89273d2949b502d2950199ad1901ff870c6ebf78
Jason Tian <tianjason6@gmail.com>
2021-01-10 15:31:53 -0500

    test build

--- src/GlobalVar.js
+++
@@ -1,4 +0,0 @@
-
-export const URL_SIGN_UP = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyDICnZMnrvISneUWxo-WfyjCbRj5CMuC2Y';
-export const URL_VERIFY_EMAIL = 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/getOobConfirmationCode?key=AIzaSyDICnZMnrvISneUWxo-WfyjCbRj5CMuC2Y';
-export const CONTACT_US_CLOUDFUNCTION = "https://us-central1-utsc-projects.cloudfunctions.net/sendmail";
